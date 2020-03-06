81b137d8f1b15748485e16b3342bc3ccdf463f2f
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-06 04:00:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,6 +49,15 @@ class Dog
     dog
   end
 
+  def self.new_from_db(row)
+    attributes_hash = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+    self.new(attributes_hash)
+  end
+
   def self.find_by_id(id)
     sql = <<-SQL
       SELECT *
